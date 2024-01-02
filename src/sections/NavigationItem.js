7461a378c90ddcd4602e24f0d2eb1bc4479e2771
Craig Davison <craig@Craigs-Intel-MacBook-Pro.local>
2024-01-02 20:51:22 +0000

    Fixed issue where only part of mob nav clickable

--- src/sections/NavigationItem.js
+++ src/sections/NavigationItem.js
@@ -11,9 +11,10 @@ function NavigationItem({ title, url, onClick }) {
     hover:text-sky-50 duration-300
     lg:border-none
     lg:p-0 p-4
-     border-b border-slate-800"
+     border-b border-slate-800
+     flex justify-start"
     >
-      <a href={url} onClick={onClick}>
+      <a href={url} onClick={onClick} className="h-full w-full">
         {title}
       </a>
     </li>
