066cb9bb6fb744d9df2f4c1b09f0af18a0fb47d8
Craig Davison <craig@Craigs-Intel-MacBook-Pro.local>
2024-01-08 02:07:59 +0000

    fixed issue where orbitting blobs got in the way of contact links

--- src/components/Backgrouds/OrbitalColors.jsx
+++ src/components/Backgrouds/OrbitalColors.jsx
@@ -5,6 +5,7 @@ function OrbitalColors() {
     <>
       <div
         className="fixed 
+        pointer-events-none
                     z-0 
                     top-10 
                     -left-8 
@@ -14,8 +15,8 @@ function OrbitalColors() {
                     rounded-full 
                     filter blur-2xl translate(0px 0px) opacity-5 animate-blob"
       ></div>
-      <div className="fixed z-0 top-12 right-8 w-72 h-72 bg-sky-400 rounded-full filter blur-2xl translate(0px 0px)  opacity-5 animate-blob1"></div>
-      <div className="fixed z-0 bottom-8 left-36 w-96 h-96 bg-green-400 rounded-full filter blur-2xl translate(0px 0px)  opacity-5 animate-blob2"></div>
+      <div className="fixed pointer-events-none z-0 top-12 right-8 w-72 h-72 bg-sky-400 rounded-full filter blur-2xl translate(0px 0px)  opacity-5 animate-blob1"></div>
+      <div className="fixed pointer-events-none z-0 bottom-8 left-36 w-96 h-96 bg-green-400 rounded-full filter blur-2xl translate(0px 0px)  opacity-5 animate-blob2"></div>
     </>
   );
 }
